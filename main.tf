a75360a6c6c6ac2f0a80aaa8a1c390d85dd1b807
Chaitanyasingh25 <chaitanyasinghcsc25@gmail.com>
2026-05-21 20:25:26 +0530

    import

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "ap-south-1"
+# }
+
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "chaitanya-terraform-demo-bucket-2026"
+
+#   tags = {
+#     Name        = "My Terraform Bucket"
+#     Environment = "Dev"
+#   }
+# }
+
 terraform {
   required_providers {
     aws = {
@@ -11,11 +33,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "chaitanya-terraform-demo-bucket-2026"
+resource "aws_s3_bucket" "imported_bucket" {
+  bucket = "chaitanya-manual-import-bucket"
+}
 
-  tags = {
-    Name        = "My Terraform Bucket"
-    Environment = "Dev"
-  }
+import {
+  to = aws_s3_bucket.imported_bucket
+  id = "chaitanya-manual-import-bucket"
 }
