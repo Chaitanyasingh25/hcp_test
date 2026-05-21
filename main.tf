e594db285b4bb3fb663610b67921097b4b37fcd1
Chaitanyasingh25 <chaitanyasinghcsc25@gmail.com>
2026-05-21 20:18:20 +0530

    s3_bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "chaitanya-terraform-demo-bucket-2026"
+
+  tags = {
+    Name        = "My Terraform Bucket"
+    Environment = "Dev"
+  }
 }
