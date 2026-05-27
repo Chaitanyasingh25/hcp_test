9abad5c2273ebda458b22d403b768c84ed3e5dee
Chaitanyasingh25 <chaitanyasinghcsc25@gmail.com>
2026-05-27 08:29:56 +0530

    generated file

---
+++ generated.tf
@@ -0,0 +1,11 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "chaitanya-manual-import-bucket1"
+resource "aws_s3_bucket" "imported_bucket" {
+  bucket              = "chaitanya-manual-import-bucket1"
+  force_destroy       = null
+  object_lock_enabled = false
+  tags                = {}
+  tags_all            = {}
+}
--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = "ap-south-1"
-# }
-
-# resource "aws_s3_bucket" "my_bucket" {
-#   bucket = "chaitanya-terraform-demo-bucket-2026"
-
-#   tags = {
-#     Name        = "My Terraform Bucket"
-#     Environment = "Dev"
-#   }
-# }
-
 terraform {
   required_providers {
     aws = {
@@ -33,11 +11,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "imported_bucket" {
-  bucket = "chaitanya-manual-import-bucket"
-}
-
 import {
   to = aws_s3_bucket.imported_bucket
-  id = "chaitanya-manual-import-bucket"
+  id = "chaitanya-manual-import-bucket1"
 }
